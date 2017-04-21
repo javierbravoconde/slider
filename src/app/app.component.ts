dbbd2a7fc5ba72499aff196716fc2c1b600f113b
Javier Conde <Javier.Conde@ncr.com>
2017-04-21 15:44:50 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var Swiper: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+	title = 'app works!';
+	config: Object = {
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			nextButton: '.swiper-button-next-modif',
+			prevButton: '.swiper-button-prev',
+			spaceBetween: 30,
+			buttonDisabledClass: '.my-swiper-button-disabled'
+		};  
+  
+  ngOnInit() {
+  }
+  
+  private selectOrder(event) {
+
+    alert("button clicked");
+  }  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SwiperModule } from '../../node_modules/angular2-useful-swiper'; //or for angular-cli the path will be ../../node_modules/angular2-useful-swiper
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
